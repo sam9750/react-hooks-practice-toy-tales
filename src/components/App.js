4b43ab1287da1dbfff59d7cad959117ed8e29af7
sam9750 <sam.wadhwa95@gmail.com>
2022-02-07 23:45:33 -0500

    draft

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
 
+
+
+
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toy, setToy] = useState([]);
+  const toysUrl = "http://localhost:3000/toys"
+
+  useEffect(() => {
+    function fetch() {
+      fetch(toysUrl)
+        .then(r => r.json())
+        .then(toys => setToy(toys));
+    };
+  }, []);
+
+      console.log(toy)
+
+
+// we need to send our toy down as props to our toycard, send to ToyContainer first then ToyCard, make POST request in ToyForm
+
+
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
+
+
+
+
+
+
+
+
   }
 
   return (
@@ -22,5 +50,8 @@ function App() {
     </>
   );
 }
+
+
+
 
 export default App;
